88b4df812245461d5d0cb2d2127d1aff9fcef819
yuma <ikeoku.y@gmail.com>
2022-01-24 08:23:43 +0900

    githubでのログイン処理を実装した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash!(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    image_url = auth_hash[:info][:image]
+
+    User.find_or_create_by!(provider: provider, uid: uid) do |user|
+      user.image_url = image_url
+    end
+  end
 end
