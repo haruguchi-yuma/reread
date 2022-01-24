ecd0aa3b2eeec3cd1d40df5da650d89dc59a389c
yuma <ikeoku.y@gmail.com>
2022-01-24 16:17:30 +0900

    bin/rails g controller books index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :books, only: :index
 end
