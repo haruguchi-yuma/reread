276596134911f01555265741643e1fdf050a9433
yuma <ikeoku.y@gmail.com>
2022-01-25 23:37:52 +0900

    レンダリング時に@booksにnilが入ってるためエラーが起きていたので createアクションで書籍の全てのインスタンスを作成するようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
 
   def create
     @book = current_user.books.new(book_params)
+    @books = current_user.books.order(created_at: :desc)
 
     if @book.save
       redirect_to books_url, notice: '書籍を登録しました'
