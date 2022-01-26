b3085841b2d83a5c16ddc1d33f4b8ba35fd12895
yuma <ikeoku.y@gmail.com>
2022-01-26 16:23:16 +0900

    shrineの設定、ストレージの設定

---
+++ config/initializers/shrine.rb
@@ -0,0 +1,13 @@
+require 'shrine'
+require "shrine/storage/file_system"
+
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads")
+}
+
+Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data
+Shrine.plugin :restore_cached_data
+Shrine.plugin :validation
+Shrine.plugin :validation_helpers
