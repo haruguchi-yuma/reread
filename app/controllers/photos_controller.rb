0a4284641ee6883942b6cef8d90f2813d7af379f
yuma <ikeoku.y@gmail.com>
2022-01-27 16:22:22 +0900

    写真投稿機能と写真一覧ページの作成

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,23 @@
 class PhotosController < ApplicationController
   def index
+    @photo = Book.find(params[:book_id]).photos.new
+    @photos = Book.find(params[:book_id]).photos
+  end
+
+  def create
+    @photo = Book.find(params[:book_id]).photos.new(photo_params)
+    @photos = Book.find(params[:book_id]).photos.order(created_at: :desc)
+
+    if @photo.save
+      redirect_to book_photos_path, notice: '写真を投稿しました'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'photos/index'
   root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :books, only: %i(index create)
+  resources :books, only: %i(index create) do
+    resources :photos, only: %i(index create)
+  end
 end
