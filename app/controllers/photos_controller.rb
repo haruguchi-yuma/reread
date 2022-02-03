7d823302b8d2c973efa907d7fe173a5cda42e60d
yuma <ikeoku.y@gmail.com>
2022-02-03 12:23:01 +0900

    hidden_fieldを記述してファイルが選択されなかったときもphoto[image]がpostで送信されるようにした

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,10 +6,9 @@ class PhotosController < ApplicationController
   end
 
   def create
-    photo = Book.find(params[:book_id]).photos.new(photo_params)
-
-    if photo.save
-      redirect_to book_url(photo.book), notice: '写真を投稿しました'
+    @photo = Book.find(params[:book_id]).photos.new(photo_params)
+    if @photo.save
+      redirect_to book_url(@photo.book), notice: '写真を投稿しました'
     else
       render :new
     end
