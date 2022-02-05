245cc44eb96d01d4b93698430726e05caf2f9920
yuma <ikeoku.y@gmail.com>
2022-02-05 20:08:38 +0900

    テストの文言を変更した

--- spec/system/welcomes_spec.rb
+++ spec/system/welcomes_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Welcomes", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
-  scenario "ログインしていないユーザがwelcomeページを訪れること" do
+  scenario "ログインしていないユーザが / ページを表示" do
     visit root_path
 
     expect(page).to have_content "Welcome#index"
