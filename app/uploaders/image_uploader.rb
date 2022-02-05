9b1ea126b09b5ed2ab7cc259dd91790180158766
yuma <ikeoku.y@gmail.com>
2022-02-05 14:11:54 +0900

    rubocopの指摘を修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,10 @@
 
 class ImageUploader < Shrine
   Attacher.validate do
-    validate_max_size 5 * 1024 * 1024, message: "サイズは5.0MB以内で選択してください"
+    validate_max_size 5 * 1024 * 1024, message: 'サイズは5.0MB以内で選択してください'
     validate_mime_type %w[image/jpeg image/png image/webp image/tiff],
-      message: "の種類はjpeg png webp tiff のいずれかで選択してください"
+                       message: 'の種類はjpeg png webp tiff のいずれかで選択してください'
     validate_extension %w[jpg jpeg png webp tiff tif],
-      message: "の拡張子はjpg jpeg png webp tiff tifのいずれかで選択してください"
+                       message: 'の拡張子はjpg jpeg png webp tiff tifのいずれかで選択してください'
   end
 end
--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,38 +1,40 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :photo do
-    image { 
+    image do
       Rack::Test::UploadedFile.new(
-        File.join(Rails.root, 'spec/factories/test_640x320.png'),
+        Rails.root.join('spec/factories/test_640x320.png'),
         'image/png'
       )
-    }
+    end
     association :book
 
     trait :within_file_size do
-      image { 
-      Rack::Test::UploadedFile.new(
-          File.join(Rails.root, 'spec/factories/test_5MB.jpg'),
+      image do
+        Rack::Test::UploadedFile.new(
+          Rails.root.join('spec/factories/test_5MB.jpg'),
           'image/jpeg'
         )
-      }
+      end
     end
 
     trait :over_file_size do
-      image { 
+      image do
         Rack::Test::UploadedFile.new(
-          File.join(Rails.root, 'spec/factories/test_6MB.jpg'),
+          Rails.root.join('spec/factories/test_6MB.jpg'),
           'image/jpeg'
         )
-      }
+      end
     end
 
     trait :pdf do
-      image { 
+      image do
         Rack::Test::UploadedFile.new(
-          File.join(Rails.root, 'spec/factories/test.pdf'),
+          Rails.root.join('spec/factories/test.pdf'),
           'application/pdf'
         )
-      }
+      end
     end
   end
 end
