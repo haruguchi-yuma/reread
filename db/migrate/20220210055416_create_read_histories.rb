f637961dd9c9559ed9418c9e018f917222993db8
yuma <ikeoku.y@gmail.com>
2022-02-10 15:13:37 +0900

    読み返す日のカラム名を変更した

--- db/migrate/20220210055416_create_read_histories.rb
+++ db/migrate/20220210055416_create_read_histories.rb
@@ -3,7 +3,7 @@ class CreateReadHistories < ActiveRecord::Migration[7.0]
     create_table :read_histories do |t|
       t.string :summary, null: false
       t.text :description
-      t.date :history, null: false
+      t.date :read_back_at, null: false
       t.references :book, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2022_01_26_070315) do
+ActiveRecord::Schema.define(version: 2022_02_10_055416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema[6.1].define(version: 2022_01_26_070315) do
     t.index ["book_id"], name: "index_photos_on_book_id"
   end
 
+  create_table "read_histories", force: :cascade do |t|
+    t.string "summary", null: false
+    t.text "description"
+    t.date "read_back_at", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_read_histories_on_book_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", null: false
     t.string "uid", null: false
@@ -42,4 +52,5 @@ ActiveRecord::Schema[6.1].define(version: 2022_01_26_070315) do
 
   add_foreign_key "books", "users"
   add_foreign_key "photos", "books"
+  add_foreign_key "read_histories", "books"
 end
