03ed4bb8b4e9e6f947601864115d0246a2567ffd
yuma <ikeoku.y@gmail.com>
2022-02-15 19:35:34 +0900

    再読日をDBに保存できるようにした

--- app/controllers/read_histories_controller.rb
+++ app/controllers/read_histories_controller.rb
@@ -1,6 +1,26 @@
+require "google/apis/calendar_v3"
+require "googleauth"
+require "googleauth/stores/file_token_store"
+require "date"
+require "fileutils"
+
 class ReadHistoriesController < ApplicationController
   def new
     # googleカレンダー認証処理を挟む（フィルター？）
     @read_history = Book.find(params[:book_id]).read_histories.new
+  end
+
+  def create
+    @read_history = Book.find(params[:book_id]).read_histories.new(read_history_params)
+
+    if @read_history.save
+      redirect_to book_path(@read_history.book), notice: '再読日を設定しました'
+    else
+      render :new
+    end
+  end
+
+  def read_history_params
+    params.require(:read_history).permit(:summary, :read_back_at, :description)
   end
 end
