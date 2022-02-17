6c7c62a91919cf00b854535a2cc9b996aa7452ff
yuma <ikeoku.y@gmail.com>
2022-02-17 14:27:25 +0900

    omniauthの設定をした

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, ENV['GITHUB_CLIENT_ID'],  ENV['GITHUB_CLIENT_SECRET']
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'],  ENV['GOOGLE_CLIENT_SECRET']
   else
     provider :github,
       Rails.application.credentials.github[:client_id],
