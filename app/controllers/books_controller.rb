b51b5bf203efcb4fc594be2d26a96fc12790a823
yuma <ikeoku.y@gmail.com>
2022-03-19 11:44:36 +0900

    空行を1行作った

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
   before_action :allow_only_book_pages_from_your_own_book, only: %i[show edit update destroy]
+
   def index
     @book = current_user.books.new
     @books = current_user.books.order(created_at: :desc).page(params[:page])
