be8188cce3d511a4ca92988efa18764020fc0d4b
yuma <ikeoku.y@gmail.com>
2022-04-11 14:36:25 +0900

    本番環境と開発環境でclient_idなどが違うため、分岐させた

--- app/models/calendar_client.rb
+++ app/models/calendar_client.rb
@@ -2,11 +2,13 @@
 
 class CalendarClient
   def initialize(user)
+    google_client_id = Rails.env.production? ? Rails.application.credentials.google[:client_id] : ENV['GOOGLE_CLIENT_ID']
+    google_client_secret = Rails.env.production? ? Rails.application.credentials.google[:client_secret] : ENV['GOOGLE_CLIENT_SECRET']
     @service = Google::Apis::CalendarV3::CalendarService.new
     @user = user
     @client = Signet::OAuth2::Client.new(
-      client_id: ENV['GOOGLE_CLIENT_ID'],
-      client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+      client_id: google_client_id,
+      client_secret: google_client_secret,
       authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
       access_token: Rails.cache.read(user.uid),
