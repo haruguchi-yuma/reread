a8d16d6586df971a66f2936afa5484d80d7145cf
yuma <ikeoku.y@gmail.com>
2022-04-12 15:22:44 +0900

    ファイル選択時にヘッダーのロゴが消えてしまう問題を解消した

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,4 +1,4 @@
-if (document.URL.match(/new/)) {
+if (document.URL.match(/books\/\d+\/photos\/new/)) {
   document.addEventListener('DOMContentLoaded', () => {
     const createImageHTML = (blob) => {
       const imageElement = document.getElementById('new-image')
@@ -9,7 +9,8 @@ if (document.URL.match(/new/)) {
       imageElement.appendChild(blobImage)
     }
     document.getElementById('photo_image').addEventListener('change', (e) => {
-      const imageContent = document.querySelector('img')
+      const imageContent = document.querySelector('.new-img')
+      console.log(imageContent)
       if (imageContent) imageContent.remove()
 
       const file = e.target.files[0]
