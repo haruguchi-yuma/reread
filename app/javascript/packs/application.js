30a9d6d6a7dc63c5064c8521e3e2e8b514f76126
yuma <ikeoku.y@gmail.com>
2022-04-13 19:31:49 +0900

    turbolinksの設定を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,12 @@
 // that code so it'll be compiled.
 
 import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 
 require('../src/header')
 require('./preview')
 
 Rails.start()
+Turbolinks.start()
 ActiveStorage.start()
