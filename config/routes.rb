6371a1bb4cf5dc80d7783eb2425cf48c25fd5221
yuma <ikeoku.y@gmail.com>
2022-04-20 15:46:02 +0900

    不要なルーティングを削除した

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get "oauth2callback", to:"read_histories#callback"
   delete '/logout', to: 'sessions#destroy'
 
   resource :retirement
