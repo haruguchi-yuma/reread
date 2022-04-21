c4cf3ff22175ffbac482e49e53ed95441b08361d
yuma <ikeoku.y@gmail.com>
2022-04-21 13:30:48 +0900

    セッション情報を保持するsetterを定義した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id])
   end
 
-  def current_user=(user, auth_hash)
+  def current_user=(user)
     session[:user_id] = user.id
-    user.store_credentials_in_cache(auth_hash)
   end
 
   def require_logged_in
