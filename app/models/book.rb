5be59258e423579142496d3f62cd9d1f35f31dbf
yuma <ikeoku.y@gmail.com>
2022-04-21 14:39:51 +0900

    最終更新日のヘルパーをモデルに定義するようにした。モデルのロジックが絡むため。

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,12 @@ class Book < ApplicationRecord
 
   validates :title, length: { maximum: 50 }, presence: true
   validates :user, presence: true
+
+  def last_update_of_photo
+    return '' unless photos.exists?
+
+    last_updated_photo = photos.order(updated_at: :desc).first
+    I18n.l(last_updated_photo.updated_at, format: :date).to_s \
+      + "(#{ApplicationController.helpers.time_ago_in_words(last_updated_photo.updated_at)}前)"
+  end
 end
