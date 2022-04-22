e81b97d2f41fdfba75003b366eaf0275a4c84c12
yuma <ikeoku.y@gmail.com>
2022-04-22 17:14:18 +0900

    特異メソッド呼び出し時のレシーバを省略

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     uid = auth_hash[:uid]
     image_url = auth_hash[:info][:image]
 
-    User.find_or_create_by!(provider: provider, uid: uid) do |user|
+    find_or_create_by!(provider: provider, uid: uid) do |user|
       user.image_url = image_url
     end
   end
