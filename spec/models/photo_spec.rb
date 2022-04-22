debdd8b8c5681a0e72c2d66ff21cd6131acce53e
yuma <ikeoku.y@gmail.com>
2022-04-22 23:21:29 +0900

    traitの名前をリネームした

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -9,23 +9,23 @@ RSpec.describe Photo, type: :model do
   end
 
   it '5MB以上の画像であれば無効な状態であること' do
-    photo = FactoryBot.build(:photo, :within_file_size)
+    photo = FactoryBot.build(:photo, :with_small_jpeg)
     photo.valid?
     expect(photo.errors[:image]).not_to include('サイズは5.0MB以内で選択してください')
 
-    over_size_photo = FactoryBot.build(:photo, :over_file_size)
+    over_size_photo = FactoryBot.build(:photo, :with_large_jpeg)
     over_size_photo.valid?
     expect(over_size_photo.errors[:image]).to include('サイズは5.0MB以内で選択してください')
   end
 
   it '拡張子がjpg jpeg png webp tiff tif以外は無効な状態であること' do
-    photo = FactoryBot.build(:photo, :pdf)
+    photo = FactoryBot.build(:photo, :with_pdf)
     photo.valid?
     expect(photo.errors[:image]).to include('の拡張子はjpg jpeg png webp tiff tifのいずれかで選択してください')
   end
 
   it 'MINEタイプがimage/jpeg image/png image/webp image/tiff以外は無効な状態であること' do
-    photo = FactoryBot.build(:photo, :pdf)
+    photo = FactoryBot.build(:photo, :with_pdf)
     photo.valid?
     expect(photo.errors[:image]).to include('の種類はjpeg png webp tiff のいずれかで選択してください')
   end
