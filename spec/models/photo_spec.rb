c83f91f44852ad937807741317cd506395acceb4
yuma <ikeoku.y@gmail.com>
2022-04-22 23:21:50 +0900

    typoの修正

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Photo, type: :model do
     expect(photo.errors[:image]).to include('の拡張子はjpg jpeg png webp tiff tifのいずれかで選択してください')
   end
 
-  it 'MINEタイプがimage/jpeg image/png image/webp image/tiff以外は無効な状態であること' do
+  it 'MIMEタイプがimage/jpeg image/png image/webp image/tiff以外は無効な状態であること' do
     photo = FactoryBot.build(:photo, :with_pdf)
     photo.valid?
     expect(photo.errors[:image]).to include('の種類はjpeg png webp tiff のいずれかで選択してください')
