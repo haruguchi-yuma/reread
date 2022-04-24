26f8828c3b04dc490beb97ffc8936bb8b85e5a56
yuma <ikeoku.y@gmail.com>
2022-04-24 11:34:09 +0900

    〜がないときは無効であるというテストにトレイとではなく明示的にnilを渡すようにした

--- spec/factories/read_histories.rb
+++ spec/factories/read_histories.rb
@@ -5,18 +5,6 @@ FactoryBot.define do
     summary { '本のタイトル' }
     read_back_on { Time.zone.tomorrow }
     description { '' }
-    association :book
-
-    trait :without_summary do
-      summary { '' }
-    end
-
-    trait :without_read_back_on do
-      read_back_on { '' }
-    end
-
-    trait :date_is_before_today do
-      read_back_on { Time.zone.yesterday }
-    end
+    book
   end
 end
