4776faa598740eab5b6dddda5e650a9ea8f7c60a
yuma <ikeoku.y@gmail.com>
2022-04-25 09:53:20 +0900

    ログインログアウトのテストをusers_specに記述していたのでwelcomes_specに移動させた

--- spec/system/welcomes_spec.rb
+++ spec/system/welcomes_spec.rb
@@ -5,6 +5,19 @@ require 'rails_helper'
 RSpec.describe 'Welcome', type: :system do
   let(:user) { FactoryBot.create(:user) }
 
+  it 'ユーザがログインできる' do
+    sign_in_as user
+    expect(page).to have_content 'ログインしました'
+    expect(page).to have_content '読み返したい本リスト'
+  end
+
+  it 'ログインしているユーザがログアウトできる' do
+    sign_in_as user
+    click_on 'ログアウト'
+    expect(page).to have_content 'ログアウトしました'
+    expect(page).to have_content '「また後で読み返そう」そう思って忘れてしまっている本はありませんか？'
+  end
+
   it 'ログインしていないユーザが / ページを表示' do
     visit root_path
 
