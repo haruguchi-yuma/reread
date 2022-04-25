938dade29184d0b05eea8b734638615839ee06c2
yuma <ikeoku.y@gmail.com>
2022-04-25 09:49:12 +0900

    ログインしている時のテストを追加

--- spec/system/welcomes_spec.rb
+++ spec/system/welcomes_spec.rb
@@ -2,10 +2,19 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Welcomes', type: :system do
-  scenario 'ログインしていないユーザが / ページを表示' do
+RSpec.describe 'Welcome', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  it 'ログインしていないユーザが / ページを表示' do
     visit root_path
 
     expect(page).to have_content '「また後で読み返そう」そう思って忘れてしまっている本はありませんか？'
+  end
+
+  it 'ログインしているユーザが / ページを表示' do
+    sign_in_as user
+    visit root_path
+
+    expect(page).to have_content '読み返したい本リスト'
   end
 end
