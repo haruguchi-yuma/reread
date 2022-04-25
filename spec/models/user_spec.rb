4f2dd95987fd70926c5c50d2b35edb53d79c39be
yuma <ikeoku.y@gmail.com>
2022-04-25 14:15:13 +0900

    FactoryBotを省略できるように設定した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'User.find_or_create_from_auth_hash!' do
     it '有効な引数が渡されたときuserが見つかること' do
-      user = FactoryBot.create(:user)
+      user = create(:user)
       auth_hash = {provider: user.provider, uid: user.uid, info: {image: user.image_url}}
       expect(User.find_or_create_from_auth_hash!(auth_hash)).to eq user
     end
 
     it '無効な引数が渡された場合エラーが出ること' do
-      user = FactoryBot.build(:user)
+      user = build(:user)
       auth_hash = {provider: 'twitter_oauth', uid: user.uid, info: {image: user.image_url}}
       expect{User.find_or_create_from_auth_hash!(auth_hash)}.to raise_error ActiveRecord::RecordInvalid
     end
