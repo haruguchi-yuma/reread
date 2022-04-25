393b0cab349098d936b40c060f240a0824e9e573
yuma <ikeoku.y@gmail.com>
2022-04-25 10:27:08 +0900

    ユーザー名を意味がわかる名前に変更した

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -3,23 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Books', type: :system do
-  let(:user_a) { FactoryBot.create(:user) }
-  let(:user_b) { FactoryBot.create(:user) }
+  let(:login_user) { FactoryBot.create(:user) }
+  let(:not_login_user) { FactoryBot.create(:user) }
 
   describe '一覧表示機能' do
-    let!(:book) { FactoryBot.create(:book, title: '最初の書籍', user: user_a) }
+    let!(:book_of_login_user) { FactoryBot.create(:book, title: 'ログインしたユーザーが登録した本', user: login_user) }
+    let!(:book_of_not_login_user) { FactoryBot.create(:book, title: 'ログインしていないユーザーが登録した本', user: not_login_user)}
 
-    context 'ユーザーAがログインしているとき' do
-      it '登録した書籍が表示される' do
-        sign_in_as user_a
-        expect(page).to have_content '最初の書籍'
+    context 'ユーザーがログインしているとき' do
+      it 'ログインしたユーザーが登録した書籍が表示される' do
+        sign_in_as login_user
+        expect(page).to have_content 'ログインしたユーザーが登録した本'
       end
-    end
 
-    context 'ユーザーBがログインしているとき' do
-      it 'ユーザーAが登録した書籍は表示されない' do
-        sign_in_as user_b
-        expect(page).not_to have_content '最初の書籍'
+      it 'ログインしていないユーザーが登録した書籍は表示されない' do
+        sign_in_as login_user
+        expect(page).not_to have_content 'ログインしていないユーザーが登録した本'
         expect(current_path).to eq books_path
       end
     end
@@ -28,11 +27,11 @@ RSpec.describe 'Books', type: :system do
   describe '登録機能' do
     context '書籍名を入力したとき' do
       it '登録される' do
-        sign_in_as user_a
+        sign_in_as login_user
         expect do
           fill_in 'book[title]', with: '最初の書籍'
           click_button '登録する'
-        end.to change(user_a.books, :count).by(1)
+        end.to change(login_user.books, :count).by(1)
 
         expect(page).to have_selector '.notification', text: '書籍を登録しました'
         expect(page).to have_content '最初の書籍'
@@ -41,11 +40,11 @@ RSpec.describe 'Books', type: :system do
 
     context '書籍名を入力しなかったとき' do
       it 'エラーになる' do
-        sign_in_as user_a
+        sign_in_as login_user
         expect do
           fill_in 'book[title]', with: ''
           click_button '登録する'
-        end.to change(user_a.books, :count).by(0)
+        end.to change(login_user.books, :count).by(0)
 
         within '#error_explanation' do
           expect(page).to have_content '書籍名を入力してください'
@@ -55,30 +54,30 @@ RSpec.describe 'Books', type: :system do
   end
 
   describe '編集機能' do
-    let(:book) { FactoryBot.create(:book, title: '書籍名の変更', user: user_a) }
+    let(:login_users_book) { FactoryBot.create(:book, title: '書籍名の変更', user: login_user) }
 
-    context 'ユーザーAが作成したとき' do
+    context 'ログインユーザーが作成したとき' do
       it '編集できる' do
-        sign_in_as user_a
-        visit edit_book_path(book)
+        sign_in_as login_user
+        visit edit_book_path(login_users_book)
 
         expect do
           fill_in 'book[title]', with: '変更した書籍'
           click_button '更新する'
-        end.to change(user_a.books, :count).by(0)
+        end.to change(login_user.books, :count).by(0)
 
         expect(page).to have_selector '.notification', text: '「変更した書籍」に変更しました'
         expect(page).to have_content '変更した書籍'
       end
 
       it '書籍名を空文字にして更新するとエラーになる' do
-        sign_in_as user_a
-        visit edit_book_path(book)
+        sign_in_as login_user
+        visit edit_book_path(login_users_book)
 
         expect do
           fill_in 'book[title]', with: ''
           click_button '更新する'
-        end.to change(user_a.books, :count).by(0)
+        end.to change(login_user.books, :count).by(0)
 
         within '#error_explanation' do
           expect(page).to have_content '書籍名を入力してください'
@@ -88,27 +87,27 @@ RSpec.describe 'Books', type: :system do
   end
 
   describe '削除機能' do
-    let(:book) { FactoryBot.create(:book, title: '最初の書籍', user: user_a) }
+    let(:login_users_book) { FactoryBot.create(:book, title: '最初の書籍', user: login_user) }
 
     context '写真が一枚も投稿されていないとき' do
       it '削除できる', js: true do
-        sign_in_as user_a
-        visit book_path(book)
+        sign_in_as login_user
+        visit book_path(login_users_book)
 
         click_on 'この書籍を削除する'
         expect do
           expect(page.accept_confirm).to eq '投稿した写真も削除されます。よろしいですか？'
           expect(page).to have_content '「最初の書籍」を削除しました'
-        end.to change(user_a.books, :count).by(-1)
+        end.to change(login_user.books, :count).by(-1)
       end
     end
 
     context '写真が投稿されているとき' do
-      let!(:photo) { FactoryBot.create(:photo, book: book) }
+      let!(:photo) { FactoryBot.create(:photo, book: login_users_book) }
 
       it '削除できる', js: true do
-        sign_in_as user_a
-        visit book_path(book)
+        sign_in_as login_user
+        visit book_path(login_users_book)
 
         expect(page).to have_selector ".image-#{photo.id}"
         click_on 'この書籍を削除する'
@@ -116,7 +115,7 @@ RSpec.describe 'Books', type: :system do
         expect do
           expect(page.accept_confirm).to eq '投稿した写真も削除されます。よろしいですか？'
           expect(page).to have_content '「最初の書籍」を削除しました'
-        end.to change(user_a.books, :count).by(-1)
+        end.to change(login_user.books, :count).by(-1)
       end
     end
   end
