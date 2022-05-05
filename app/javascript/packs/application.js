0e7e5813abdff78bd3d598dd33ea83a3334606e2
yuma <ikeoku.y@gmail.com>
2022-05-05 09:30:38 +0900

    turbolinksを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from '@rails/ujs'
-import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 
 import('../src/header')
@@ -12,5 +11,4 @@ import('../src/modal')
 import('../preview')
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
