bcb1b2d017b48e2ccfb1196501e1d754a8eae99f
yuma <ikeoku.y@gmail.com>
2022-05-13 13:03:07 +0900

    import文を書き換えたところjsがきかなくなったのでaddEventListerを追加

--- app/javascript/src/modal.js
+++ app/javascript/src/modal.js
@@ -1,8 +1,10 @@
-for (const element of document.querySelectorAll(
-  '#show-modal, #close-modal, #modal-background, #close-button'
-)) {
-  element.addEventListener('click', () => {
-    const modal = document.querySelector('#modal-window')
-    modal.classList.toggle('is-active')
-  })
-}
+document.addEventListener('DOMContentLoaded', () => {
+  for (const element of document.querySelectorAll(
+    '#show-modal, #close-modal, #modal-background, #close-button'
+  )) {
+    element.addEventListener('click', () => {
+      const modal = document.querySelector('#modal-window')
+      modal.classList.toggle('is-active')
+    })
+  }
+})
--- app/javascript/src/preview.js
+++ app/javascript/src/preview.js
@@ -1,18 +1,20 @@
-const createImageHTML = (blob) => {
-  const imageElement = document.querySelector('#new-image')
-  const blobImage = document.createElement('img')
-  blobImage.setAttribute('class', 'new-img')
-  blobImage.setAttribute('src', blob)
+document.addEventListener('DOMContentLoaded', () => {
+  const createImageHTML = (blob) => {
+    const imageElement = document.querySelector('#new-image')
+    const blobImage = document.createElement('img')
+    blobImage.setAttribute('class', 'new-img')
+    blobImage.setAttribute('src', blob)
 
-  imageElement.appendChild(blobImage)
-}
+    imageElement.appendChild(blobImage)
+  }
 
-document.querySelector('#photo_image')?.addEventListener('change', (e) => {
-  const imageContent = document.querySelector('.new-img')
-  imageContent && imageContent.remove()
+  document.querySelector('#photo_image')?.addEventListener('change', (e) => {
+    const imageContent = document.querySelector('.new-img')
+    imageContent && imageContent.remove()
 
-  const file = e.target.files[0]
-  const blob = window.URL.createObjectURL(file)
-  console.log(blob)
-  createImageHTML(blob)
+    const file = e.target.files[0]
+    const blob = window.URL.createObjectURL(file)
+    console.log(blob)
+    createImageHTML(blob)
+  })
 })
