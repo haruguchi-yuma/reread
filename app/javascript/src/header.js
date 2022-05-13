5daafbbde5dc28b7c1585fbd53f7a2c9d567095d
yuma <ikeoku.y@gmail.com>
2022-05-13 12:48:37 +0900

    不要な条件分岐を削除

--- app/javascript/src/header.js
+++ app/javascript/src/header.js
@@ -1,15 +1,13 @@
 document.addEventListener('turbolinks:load', () => {
   const navbarBurgers = document.querySelectorAll('.navbar-burger')
 
-  if (navbarBurgers.length > 0) {
-    navbarBurgers.forEach((el) => {
-      el.addEventListener('click', () => {
-        const target = el.dataset.target
-        const $target = document.getElementById(target)
+  navbarBurgers.forEach((el) => {
+    el.addEventListener('click', () => {
+      const target = el.dataset.target
+      const $target = document.getElementById(target)
 
-        el.classList.toggle('is-active')
-        $target.classList.toggle('is-active')
-      })
+      el.classList.toggle('is-active')
+      $target.classList.toggle('is-active')
     })
-  }
+  })
 })
